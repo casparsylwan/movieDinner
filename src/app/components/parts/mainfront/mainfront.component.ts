12ff5361d8f4315918ad0c66727b9848539853c7
casparsylwan <casparsylwan@gmail.com>
2020-04-04 10:45:38 +0200

    sideways scrool trail one

--- src/app/components/parts/mainfront/mainfront.component.ts
+++ src/app/components/parts/mainfront/mainfront.component.ts
@@ -9,6 +9,24 @@ export class MainfrontComponent implements OnInit {
 
   fronttext:string = " Bio middag enjoy! "
 
+  imges:string[] = ["/assets/sources/img/small/alfonsåberg.png",
+                    "/assets/sources/img/small/avatar.png",
+                    "/assets/sources/img/small/forestgump.png",
+                    "/assets/sources/img/small/harrypotter.png",
+                    "/assets/sources/img/small/matrix.png",
+                    "/assets/sources/img/small/zoo.png"] 
+
+  bgImage:string ="/assets/sources/img/small/alfonsåberg.png";
+    
+    /*
+    C:\Users\Caspars Dator\Desktop\fullstack\moviedinner\movieDinner\src\app\sources\img\small\alfonsåberg.png
+    'marginTop': !(spinTheLines) ? '16px' : '7px',
+    'transform': !(spinTheLines) ? 'rotate(225deg)' : 'rotate(0deg)' }
+
+
+
+    
+    */
   constructor() { }
 
   ngOnInit(): void {
