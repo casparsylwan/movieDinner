de222ee2d70b58be1a34266383bf629c6fd5fe5a
casparsylwan <casparsylwan@gmail.com>
2020-04-04 12:17:13 +0200

    scroooool and non local img

--- src/app/components/parts/mainfront/mainfront.component.ts
+++ src/app/components/parts/mainfront/mainfront.component.ts
@@ -9,12 +9,12 @@ export class MainfrontComponent implements OnInit {
 
   fronttext:string = " Bio middag enjoy! "
 
-  imges:string[] = ["/assets/sources/img/small/alfonsåberg.png",
-                    "/assets/sources/img/small/avatar.png",
-                    "/assets/sources/img/small/forestgump.png",
-                    "/assets/sources/img/small/harrypotter.png",
-                    "/assets/sources/img/small/matrix.png",
-                    "/assets/sources/img/small/zoo.png"] 
+  imges:string[] = ["https://m.media-amazon.com/images/M/MV5BOGYyNWQ1MDItYzFjNC00NmVjLTlkZDQtNDg0YmI1YzAzYjVkXkEyXkFqcGdeQXVyMTMwOTMzNzU@._V1_.jpg",
+                    "https://m.media-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_.jpg",
+                    "https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
+                    "https://m.media-amazon.com/images/M/MV5BMTI1NDMyMjExOF5BMl5BanBnXkFtZTcwOTc4MjQzMQ@@._V1_.jpg",
+                    "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,665,1000_AL_.jpg",
+                    "https://m.media-amazon.com/images/M/MV5BYWY2ZTcxY2QtOGNjMS00ZGI5LTliOTYtNDU3ZTcxMzVjYzU2XkEyXkFqcGdeQXVyNjA3NjE4OTc@._V1_.jpg"] 
 
   bgImage:string ="/assets/sources/img/small/alfonsåberg.png";
     
